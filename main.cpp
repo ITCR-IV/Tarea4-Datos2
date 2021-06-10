81e33b6211f6537e189af83dc43171811e7bdd0a
Apollo13-0 <calderon.ig08@gmail.com>
2021-06-10 00:57:51 -0600

    add MD5 logic

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include "SHA1/SHA1.h"
+#include "MD5/MD5.h"
 
 int main()
 {
     SHA1("123456789 10 11 12\n");
+
+    std::string word;
+    std::cout << "Ingrese la palabra por encriptar (MD5): ";
+    std::cin>> word;
+    std::cout << word << ": "<< md5(word) << std::endl;
 
     return 0;
 }
